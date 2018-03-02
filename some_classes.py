4cbe0a45507f53e3ba204f98b327623127d5d16b
CoreyCunningham <coreyjjc@me.com>
2018-03-02 11:31:27 -0500

    store

--- some_classes.py
+++ some_classes.py
@@ -57,6 +57,80 @@ class Car(object):
         print("Mileage: " + str(self.mileage) + "mpg")
         print("Tax: " + str(self.tax))
         return self
+#
+# car1 = Car(20000,35,"Full",105)
+# car1.display_all()
 
-car1 = Car(20000,35,"Full",105)
-car1.display_all()
+class Product(object):
+    def __init__(self,price,item_name,weight,brand):
+        self.price = price
+        self.item_name = item_name
+        self.weight = weight
+        self.brand = brand
+        self.status = "for sale"
+
+    def sell(self):
+        """
+        change self.status -> 'sold'
+        return self
+        """
+        self.status = "sold"
+        return self
+
+    def add_tax(self,tax):
+        """
+        returns the price w tax
+        """
+        return self.price + (self.price * tax)
+
+    def return_item(self,reason):
+        """
+        if reason=defective, change status to defective, price to 0
+        if reason=unopened, change status to for sale
+        if reason=opened, change status to used, price = price - 20%
+        return self
+        """
+        if reason == "defective":
+            self.status = "defective"
+            self.price = 0
+        elif reason == "unopened":
+            self.status = "for sale"
+        else:
+            self.status = "used"
+            self.price -= (.20 * self.price)
+        return self
+
+
+cups = Product(10,"cups",1,"GE")
+plates = Product(15,"plates",2,"NA")
+knives = Product(2,"knives",1,"BBB")
+
+class Store(object):
+    def __init__(self):
+        self.products = []
+        self.location = ""
+        self.owner = ""
+    def set_owner(self,owner):
+        self.owner = owner
+        return self
+    def set_location(self,location):
+        self.location = location
+    def add_product(self,product):
+        self.products.append(product)
+        return self
+    def remove_product(self,product):
+        index = self.products.index(product)
+        del(self.products[index])
+        return self
+
+    def inventory(self):
+        for product in self.products:
+            print("Name: {x}, Price: {y}, Status: {z}".format(
+                x=product.item_name, y=product.price, z=product.status
+            ))
+
+# our_store = Store()
+# our_store.add_product(cups).add_product(knives).add_product(plates)
+# our_store.set_owner("corey c")
+# our_store.set_owner("washington dc")
+# our_store.inventory()
